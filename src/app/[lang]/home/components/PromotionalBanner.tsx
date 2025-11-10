5c599c0ce57352db410b0c7ac01de5c5848fde1c
CarlosManuel Crespo Astorac <carlosmca4424@gmail.com>
2025-11-10 05:57:38 +0100

    fix(PromotionalBanner and PromotionalPayBanner):adjustments to both

--- src/app/[lang]/home/components/PromotionalBanner.tsx
+++ src/app/[lang]/home/components/PromotionalBanner.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function PromotionalBanner() {
   return (
     <section className="w-full bg-transparent py-4 mt-3">
-      <div className="container mx-auto flex flex-col-reverse lg:flex-row lg:items-center gap-5 px-5">
+      <div className="container mx-auto flex flex-col-reverse lg:flex-row lg:justify-between lg:items-center gap-5 px-5">
         {/* Imagen */}
         <div className="w-full lg:w-1/2 relative rounded-lg overflow-hidden aspect-[1.2] lg:aspect-[1.1]">
           <Image
@@ -19,11 +19,11 @@ export default function PromotionalBanner() {
 
         {/* Texto */}
         <div className="flex-1 flex flex-col items-start text-center h-auto md:text-left lg:w-1/2">
-          <p className="text-3xl md:text-4xl lg:text-6xl font-bold mb-4">
+          <p className="text-[35px] lg:text-[70px] font-bold mb-3 leading-tight">
             <span className="text-[#222]"> ¿Qué hacemos </span>
             <span className="text-[#2c8254]"> en BrincoXpress?</span>
           </p>
-          <p className="text-gray-700 mb-4 text-justify lg:text-[14px]">
+          <p className="text-[#757878] text-[15px] mb-4 text-justify">
             Somos una tienda de ventas online que oferta una amplia gama de
             productos de calidad desde alimentos básicos, productos de aseo y
             limpieza, electrodomésticos, herramientas de ferretería, hasta
@@ -37,7 +37,7 @@ export default function PromotionalBanner() {
           </p>
 
           <div>
-            <button className="bg-[#2c8254] h-auto w-[150px] hover:bg-olive-700 text-white text-xl lg:text-2xl lg:w-[170px] py-2 px-6 rounded-full transition-all duration-200">
+            <button className="bg-[#2c8254] h-auto w-[150px] hover:bg-olive-700 text-white text-[17px]  py-2 px-6 rounded-full transition-all duration-200">
               <Link className="text-white!" href={"#"}>
                 Ver más
               </Link>
--- src/app/[lang]/home/components/PromotionalPayBanner.tsx
+++ src/app/[lang]/home/components/PromotionalPayBanner.tsx
@@ -6,20 +6,20 @@ import Link from "next/link";
 export default function PromotionalPayBanner() {
   return (
     <section className="w-full bg-transparent py-4 mt-3">
-      <div className="container mx-auto flex flex-col lg:flex-row lg:items-center gap-2 px-5">
+      <div className="container mx-auto flex flex-col lg:flex-row lg:justify-between lg:items-center gap-2 px-5">
         <div className="flex-1 flex flex-col items-start text-center md:text-left lg:w-1/2">
-          <p className="text-3xl md:text-4xl lg:text-6xl font-bold mb-4">
+          <p className="text-[35px]  lg:text-[70px] font-bold mb-4 leading-tight">
             <span className="text-[#222]"> ¿Cómo pagar en </span>
             <span className="text-[#2c8254]">BrincoXpress?</span>
           </p>
-          <p className="text-gray-700 mb-4 text-justify lg:text-[14px]">
+          <p className="text-[#757878] text-[15px] mb-4 text-justify">
             Para realizar el pago en BrincoXpress, completa los campos
             requeridos en el checkout y confirme su pago, utilizando su método
             de pago preferido.
           </p>
 
           <div>
-            <button className="bg-[#2c8254] h-auto w-[150px] hover:bg-olive-700 text-white text-xl lg:text-2xl lg:w-[170px] py-2 px-6 rounded-full transition-all duration-200">
+            <button className="bg-[#2c8254] h-auto w-[150px] hover:bg-olive-700 text-white text-[17px] py-2 px-6 rounded-full transition-all duration-200">
               <Link className="text-white!" href={"#"}>
                 Ver más
               </Link>
@@ -31,8 +31,8 @@ export default function PromotionalPayBanner() {
           <Image
             src="/assets/images/banner/bn2_pay_image.webp"
             alt="Imagen del banner"
-            width={1920} // mitad del ancho original
-            height={2221} // mantiene proporción
+            width={1920}
+            height={2221}
             className="w-full h-auto rounded-lg"
           />
         </div>
